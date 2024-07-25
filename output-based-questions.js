257a10d777aaed25f8d9aa78e1ed65db7b8d8719
Sushant Bahirat <69125526+sushant1123@users.noreply.github.com>
2024-07-25 13:27:56 +0530

    output-based-questions

---
+++ output-based-questions.js
@@ -0,0 +1,128 @@
+// Problem 1:
+
+console.log("begins"); // 1
+
+setTimeout(() => {
+  console.log("set timeout 1"); 	// 3
+  Promise.resolve().then(() => {
+    console.log("promise 1"); 	// 4
+  })
+}, 0)
+
+new Promise(function(resolve, reject) {
+  console.log("promise 2");  // 2
+  setTimeout(function() {
+    console.log("set timeout 2");  	// 5
+    resolve("resolve 1")
+  }, 0)
+}).then((res) => {
+  console.log("dot then 1");  	// 6 
+  setTimeout(() => {
+    console.log(res);  	// 7
+  }, 0)
+})
+
+// Problem 2
+async function async1() {
+  console.log("async1 start"); // 2
+  await async2();
+  console.log("async1 end"); // 6
+}
+
+async function async2() {
+  console.log("async2"); // 3
+}
+
+console.log("script start"); // 1
+
+setTimeout(() => {
+  console.log("set timeout"); // 8
+}, 0)
+
+async1();
+
+new Promise(function(resolve, reject) {
+  console.log("promise"); // 4
+  resolve();
+}).then(() => {
+  console.log("resolved promise"); // 7
+})
+
+console.log("script end"); // 5
+
+
+// Problem 3:
+async function async1() {
+  console.log("async1 start"); // 2
+  await async2();
+  console.log("async1 end"); // 6
+}
+
+async function async2() {
+  console.log("async2"); // 3
+}
+
+console.log("script start"); // 1
+
+setTimeout(() => {
+  console.log("set timeout"); // 7
+}, 0)
+
+async1();
+
+new Promise(function(resolve, reject) {
+  console.log("promise"); // 4
+  resolve();
+}).then(() => {
+  setTimeout(() => {
+    console.log("resolved promise"); // 8
+  }, 0)
+})
+
+console.log("script end"); // 5
+
+
+// Problem 4:
+var obj = {
+  name: "Sush",
+  sing: function() {
+    this.age = 20;
+    console.log("a", this.age); // 20
+    var anotherFn = function() {
+      this.age = 30;
+      console.log("b", this.age); // 30
+    }
+
+    anotherFn();
+    console.log("c", this.age); // 20
+  }
+
+}
+
+obj.sing();
+
+
+// Problem 5:
+var obj = {
+  name: "Sush",
+  sing: function() {
+    this.age = 20;
+    console.log("a", this); // refers to the scope of obj object
+    var anotherFn = function() {
+      this.age = 30;
+      console.log("b", this); 
+      // it is a function expression and there is no context assigned to it by default as there is no specific object associated with it. 
+      // So, JavaScript assigns the default 'this' value, which is the global window object.
+    }
+
+    anotherFn();
+    console.log("c", this); // refers to the scope of obj object
+  }
+
+}
+
+obj.sing();
+
+
+
+
